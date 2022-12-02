ca98068111f28292488af31bfe9e7e5375a2fc21
チャン スギ <junjunchan64@gmail.com>
2022-12-02 16:22:48 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -10,90 +10,189 @@ struct CELL {
 	struct CELL* next = nullptr;
 };
 
+//関数のプロトタイプ宣言
 void create(CELL* currentCell, const char* str);
 void Chenge(CELL* currentCell, const char* str);
+void Delete(CELL* currentCell);
+void DeleteChangeNull(CELL* currentCell);
 void index(CELL* endCell);
+void indexOne(CELL* endCell, int ite);
+const char* ReturnStr(CELL* endCell);
 CELL* getInsertListAddress(CELL* Cell, int ite);
 int CellNum(CELL* endCell);
 
-int menuNum = 0;
-
 int main() {
-
+	//イテレーター
 	int iterator;
-	int inputValue;
+	//アドレス取得用
 	CELL* indsertCell;
-
+	//入力用
 	char str[128];
+	//最初の要素を作成
 	CELL head;
 	head.next = nullptr;
 	head.prev = nullptr;
+	//リストの操作用
+	bool isLoop;
+	int backNum;
+	int menuNum = 0;
+
+	//最初に要素を入れておく(3つ)
+	indsertCell = getInsertListAddress(&head, 0);
+	create(indsertCell, "banana");
+	indsertCell = getInsertListAddress(&head, 1);
+	create(indsertCell, "apple");
+	indsertCell = getInsertListAddress(&head, 2);
+	create(indsertCell, "orange");
 
 	while (true) {
+		//初期画面
 		printf("[要素の操作]\n");
 		printf("1.要素の表示\n");
-		printf("2.要素の編集\n");
-		printf("3.要素の追加\n");
-		printf("4.要素の削除\n");
+		printf("2.要素の追加\n");
+		if (CellNum(&head) != 0) {
+			printf("3.要素の編集\n");
+			printf("4.要素の削除\n");
+		}
+		printf("\n------------------------\n");
+		printf("操作を選択してください\n");
+
 		scanf_s("%d", &menuNum);
 		system("cls");
+		isLoop = true;
 		switch (menuNum)
 		{
 		case 1:
-			printf("[要素の表示]\n");
-			printf("1.要素の一覧表示\n");
-			printf("9.要素操作に戻る\n");
+			while (isLoop) {
+				//要素の表示
+				printf("[要素の表示]\n");
+				printf("1.要素の一覧表示\n");
+				printf("2.順番を指定して要素を表示\n");
+				printf("9.要素操作に戻る\n");
+				printf("\n操作を選択してください\n");
 
-			scanf_s("%d", &menuNum);
-			system("cls");
-
-			switch (menuNum)
-			{
-			case 1:
-				index(&head);
-				printf("------------------------------\n");
-				printf("要素数　: %d\n", CellNum(&head));
-
-				system("pause");
+				scanf_s("%d", &menuNum);
 				system("cls");
-				break;
 
-			case 9:
-				system("cls");
-				break;
+				switch (menuNum)
+				{
+				case 1:
+					//要素の一覧表示
+					printf("[要素の一覧表示]\n");
+					printf("要素一覧:{\n");
+					index(&head);
+					printf("}\n\n");
+					printf("要素数　: %d\n", CellNum(&head));
+					printf("------------------------------\n");
+					printf("1.要素の表示に戻る\n");
+					printf("2.要素の操作に戻る\n");
+					scanf_s("%d", &backNum);
+					if (backNum == 2) {
+						isLoop = false;
+					}
 
-			default:
-				break;
+					system("cls");
+					break;
+
+				case 2:
+					//要素の指定
+					printf("表示したい要素の順番を指定してください\n");
+					scanf_s("%d", &iterator);
+
+					printf("------------------------------\n");
+					indexOne(&head, iterator);
+					printf("------------------------------\n");
+					printf("1.要素の表示に戻る\n");
+					printf("2.要素の操作に戻る\n");
+					scanf_s("%d", &backNum);
+					if (backNum == 2) {
+						//ループを抜けて初期画面に戻る
+						isLoop = false;
+					}
+					system("cls");
+					break;
+
+				case 9:
+					//ループを抜けて初期画面に戻る
+					system("cls");
+					isLoop = false;
+					break;
+
+				default:
+					system("cls");
+					break;
+				}
+
+
 			}
 			break;
-
 		case 2:
-			printf("何番目のセルを変更するか\n");
+			//要素の挿入
+			printf("[リスト要素の挿入]\n");
+			printf("要素を追加する場所を指定してください。(最後尾に入力する場合は%d以上を入力してください)\n",CellNum(&head));
 			scanf_s("%d", &iterator);
 
-			printf("文字を入力\n");
-			scanf_s("%s", str, sizeof(head.str));
-
-
-
-			system("cls");
-			break;
-
-		case 3:
-			printf("何番目のセルの後ろに入力するか\n");
-			scanf_s("%d", &iterator);
-
-			printf("文字を入力\n");
+			printf("追加する要素の値を入力してください\n");
 			scanf_s("%s", str, sizeof(head.str));
 
 			indsertCell = getInsertListAddress(&head, iterator);
 			create(indsertCell, str);
 
-			index(&head);
+			printf("要素\"%s\"が%d番目に挿入されました\n", str, iterator);
+			system("pause");
+			system("cls");
+			break;
+
+		case 3:
+			//要素の編集
+			printf("[要素の編集]\n");
+			printf("編集したい要素の番号を指定してください。\n");
+			scanf_s("%d", &iterator);
+
+			if (CellNum(&head) >= iterator) {
+				printf("%d番目の要素の変更する値を入力してください。\n",iterator);
+				scanf_s("%s", str, sizeof(head.str));
+
+				indsertCell = getInsertListAddress(&head, iterator + 1);
+				Chenge(indsertCell, str);
+
+				printf("%d番目の要素の値が\"%s\"に変更されました。\n", iterator, str);
+				system("pause");
+			}
+			else
+			{
+				printf("%d番目の要素は見つかりませんでした。\n", iterator);
+				system("pause");
+			}
+
 			system("cls");
 			break;
 
 		case 4:
+			//要素の削除
+			printf("[要素の削除]\n");
+			printf("何番目のセルを削除するか\n");
+			scanf_s("%d", &iterator);
+
+			if (CellNum(&head) > iterator + 1) {
+				indsertCell = getInsertListAddress(&head, iterator + 1);
+				printf("%d番目の要素\"%s\"を削除しました。\n", iterator, ReturnStr(indsertCell));
+				indsertCell = getInsertListAddress(&head, iterator);
+				Delete(indsertCell);
+			}
+			else if (CellNum(&head) == iterator + 1) {
+				indsertCell = getInsertListAddress(&head, iterator + 1);
+				printf("%d番目の要素\"%s\"を削除しました。\n", iterator, ReturnStr(indsertCell));
+				indsertCell = getInsertListAddress(&head, iterator);
+				DeleteChangeNull(indsertCell);
+			}
+			else {
+				printf("%d番目の要素が見つかりませんでした。\n", iterator);
+			}
+
+
+			system("pause");
+
 
 			system("cls");
 			break;
@@ -103,9 +202,12 @@ int main() {
 		}
 	}
 
+	system("pause");
 	return 0;
 }
 
+
+//プロトタイプ関数の中身
 void create(CELL* currentCell, const char* str)
 {
 	CELL* newCell;
@@ -126,15 +228,45 @@ void Chenge(CELL* currentCell, const char* str)
 	strcpy_s(currentCell->str, sizeof(currentCell->str), str);
 }
 
+void Delete(CELL* currentCell)
+{
+	currentCell->next = currentCell->next->next;
+	currentCell->next->next->prev = currentCell;
+}
+
+void DeleteChangeNull(CELL* currentCell)
+{
+	currentCell->next = nullptr;
+}
+
 void index(CELL* endCell)
 {
-	int no = 1;
+	int no = 0;
 	while (endCell->next != nullptr) {
 		endCell = endCell->next;
 		printf("%d : ", no);
 		printf("%s\n", endCell->str);
 		no++;
 	}
+}
+
+void indexOne(CELL* endCell, int ite)
+{
+	int no = 0;
+	while (endCell->next != nullptr) {
+		endCell = endCell->next;
+		if (ite == no) {
+			printf("%d : ", no);
+			printf("%s\n", endCell->str);
+			break;
+		}
+		no++;
+	}
+}
+
+const char* ReturnStr(CELL* endCell)
+{
+	return endCell->str;
 }
 
 CELL* getInsertListAddress(CELL* Cell, int ite)
@@ -154,138 +286,7 @@ int CellNum(CELL* endCell) {
 	int num = 0;
 	while (endCell->next != nullptr) {
 		num++;
+		endCell = endCell->next;
 	}
 	return num;
 }
-
-//void MainMenu()
-//{
-//	printf("[要素の操作]\n");
-//	printf("1.要素の表示\n");
-//	printf("2.要素の編集\n");
-//	printf("3.要素の追加\n");
-//	printf("4.要素の削除\n");
-//	int num;
-//	scanf_s("%d", &num);
-//
-//	switch (num)
-//	{
-//	case 1:
-//		pFunc = &ListViewMenu;
-//		system("cls");
-//		break;
-//
-//	case 2:
-//		pFunc = &EditFactor;
-//		system("cls");
-//		break;
-//
-//	case 3:
-//		pFunc = &AddFactor;
-//		system("cls");
-//		break;
-//
-//	case 4:
-//		pFunc = &SubFactor;
-//		system("cls");
-//		break;
-//
-//	default:
-//		break;
-//	}
-//}
-//
-//void ListViewMenu()
-//{
-//	printf("[要素の表示]\n");
-//	printf("1.要素の一覧表示\n");
-//	printf("9.要素操作に戻る\n");
-//
-//	scanf_s("%d", &menuNum);
-//
-//	switch (menuNum)
-//	{
-//	case 1:
-//		pFunc = &ListView;
-//		system("cls");
-//		break;
-//
-//	case 9:
-//		pFunc = &MainMenu;
-//		system("cls");
-//		break;
-//
-//	default:
-//		break;
-//	}
-//}
-//
-//void ListView()
-//{
-//	printf("\n要素数 : %d \n\n");
-//
-//	printf("---------------\n");
-//	printf("1.要素の表示に戻る\n");
-//	printf("2.要素の操作に戻る\n");
-//	scanf_s("%d", &menuNum);
-//
-//	switch (menuNum)
-//	{
-//	case 1:
-//		pFunc = &ListViewMenu;
-//		system("cls");
-//		break;
-//
-//	case 2:
-//		pFunc = &MainMenu;
-//		system("cls");
-//		break;
-//
-//	default:
-//		break;
-//	}
-//}
-//
-//void EditFactor()
-//{
-//
-//	printf("編集する場所(最後の場所は%d)\n");
-//	int addNum;
-//	scanf_s("%d", &addNum);
-//
-//
-//	printf("変更するワード\n");
-//	char newWord[11];
-//	scanf_s("%10s", newWord, 11);
-//
-//	pFunc = &MainMenu;
-//	system("cls");
-//}
-//
-//void AddFactor()
-//{
-//
-//	printf("入れる場所(最後の場所は%d)\n");
-//	int addNum;
-//	scanf_s("%d", &addNum);
-//
-//
-//	printf("入れるワード\n");
-//	char newWord[11];
-//	scanf_s("%10s", newWord,11);
-//
-//	pFunc = &MainMenu;
-//	system("cls");
-//}
-//
-//void SubFactor()
-//{
-//	printf("消す場所(最後の場所は%d)\n");
-//	int addNum;
-//	scanf_s("%d", &addNum);
-//
-//	printf("消したワード\n");
-//
-//	pFunc = &MainMenu;
-//	system("cls");
-//}
